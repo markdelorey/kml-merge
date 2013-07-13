bd6bc1fb031dce6e41bc5a2fa8abaf5f2705c693
Mark Delorey <mark.delorey@gmail.com>
2013-07-13 10:34:47 -0600

    Project start

---
+++ merge-php/merge-il-outside.php
@@ -0,0 +1,193 @@
+<?php
+
+ini_set('memory_limit', '4096M');
+ini_set('auto_detect_line_endings', true);
+
+$csvs	=	scandir('merge');
+
+$ofile	=	'output.csv';
+$ohandle	=	fopen($ofile,'w');
+
+$totalcount	=	0;
+
+$include	=	array(
+	'538',	// Wisconsin
+	'535',
+	'531',
+	'638',	// Missouri
+	'637',
+	'636',
+	'634',
+	'633',
+	'630',
+	'631',
+	'520',	// Iowa
+	'527',
+	'528',
+	'526',
+	'463',	// Indiana
+	'464',
+	'479',
+	'478',
+	'475',
+	'476',
+	'477',
+	'424',	// Kentucky
+	'420',
+	'423'
+);
+
+$ignore	=	array(
+	'402', // Louisville
+	'401',
+	'405',
+	'400',
+	'403',
+	'630', // Missouri
+	'631',
+	'633',
+	'622',
+	'634',
+	'641',
+	'649',
+	'647',
+	'640',
+	'648',
+	'656',
+	'657',
+	'727', // Arkansas
+	'762',
+	'729',
+	'722',
+	'719',
+	'718',
+	'392', // Mississippi
+	'396',
+	'394',
+	'393',
+	'243', // Virginia
+	'240',
+	'241',
+	'245',
+	'239',
+	'230',
+	'238',
+	'232',
+	'227',
+	'224',
+	'221',
+	'208',
+	'220',
+	'222',
+	'223',
+	'201',
+	'226',
+	'225',
+	'236',
+	'237',
+	'235',
+	'279', // North Carolina
+	'285',
+	'278',
+	'276',
+	'277',
+	'275',
+	'283',
+	'272',
+	'273',
+	'274',
+	'271',
+	'286',
+	'301', // Georgia
+	'311',
+	'399',
+	'300',
+	'306',
+	'308',
+	'309',
+	'312',
+	'302',
+	'310',
+	'313',
+	'314',
+	'304',
+	'317',
+	'398',
+	'319',
+	'318',
+	'355', // Alabama
+	'351',
+	'352',
+	'350',
+	'362',
+	'368',
+	'364',
+	'365',
+	'366',
+	'363',
+	'369',
+	'361',
+	'360',
+	'367',
+	'354'
+);
+
+$headers	=	array(
+	'Route ID',
+	'ZipCRID',
+	'Zip Code',
+	'Zip3',
+	'Route Code',
+	'Route Name',
+	'State',
+	'lat',
+	'lon',
+	'geometry'
+);
+
+fputcsv($ohandle,$headers);
+
+foreach($csvs as $csv) {
+	
+	$ihandle	=	fopen('merge/'.$csv,'r');
+	
+	$count	=	0;
+	
+	while( !feof($ihandle) ) {
+	
+		$row	=	fgetcsv($ihandle);
+		
+		if($count != 0) {
+		
+			$zip3	=	substr($row[3],0,3);
+			
+			if( in_array($zip3, $include) ) {
+		
+				$out	=	array(
+					$row[1],
+					$row[2],
+					$row[3],
+					$zip3,
+					$row[4],
+					$row[5],
+					$row[6],
+					$row[9],
+					$row[10],
+					$row[17]
+				);
+			
+				fputcsv($ohandle,$out);
+			}
+		}
+		
+		
+		$count++;
+		$totalcount++;
+	}
+	
+}
+
+
+fclose($ohandle);
+
+?>
---
+++ merge-php/merge-mo-outside.php
@@ -0,0 +1,195 @@
+<?php
+
+ini_set('memory_limit', '4096M');
+ini_set('auto_detect_line_endings', true);
+
+$csvs	=	scandir('merge');
+
+$ofile	=	'output.csv';
+$ohandle	=	fopen($ofile,'w');
+
+$totalcount	=	0;
+
+$include	=	array(
+	'743',	// Oklahoma
+	'380',	// Tennessee
+	'382',
+	'684',	// Nebraska
+	'683',
+	'660',	// Kansas
+	'661',
+	'662',
+	'667',
+	'673',
+	'671',
+	'526',	// Iowa
+	'525',
+	'500',
+	'501',
+	'508',
+	'516',
+	'623',	// Illinois
+	'620',
+	'622',
+	'629',
+	'420',	// Kentucky
+	'727',	// Arkansas
+	'726',
+	'725',
+	'724'
+);
+
+$ignore	=	array(
+	'402', // Louisville
+	'401',
+	'405',
+	'400',
+	'403',
+	'630', // Missouri
+	'631',
+	'633',
+	'622',
+	'634',
+	'641',
+	'649',
+	'647',
+	'640',
+	'648',
+	'656',
+	'657',
+	'727', // Arkansas
+	'762',
+	'729',
+	'722',
+	'719',
+	'718',
+	'392', // Mississippi
+	'396',
+	'394',
+	'393',
+	'243', // Virginia
+	'240',
+	'241',
+	'245',
+	'239',
+	'230',
+	'238',
+	'232',
+	'227',
+	'224',
+	'221',
+	'208',
+	'220',
+	'222',
+	'223',
+	'201',
+	'226',
+	'225',
+	'236',
+	'237',
+	'235',
+	'279', // North Carolina
+	'285',
+	'278',
+	'276',
+	'277',
+	'275',
+	'283',
+	'272',
+	'273',
+	'274',
+	'271',
+	'286',
+	'301', // Georgia
+	'311',
+	'399',
+	'300',
+	'306',
+	'308',
+	'309',
+	'312',
+	'302',
+	'310',
+	'313',
+	'314',
+	'304',
+	'317',
+	'398',
+	'319',
+	'318',
+	'355', // Alabama
+	'351',
+	'352',
+	'350',
+	'362',
+	'368',
+	'364',
+	'365',
+	'366',
+	'363',
+	'369',
+	'361',
+	'360',
+	'367',
+	'354'
+);
+
+$headers	=	array(
+	'Route ID',
+	'ZipCRID',
+	'Zip Code',
+	'Zip3',
+	'Route Code',
+	'Route Name',
+	'State',
+	'lat',
+	'lon',
+	'geometry'
+);
+
+fputcsv($ohandle,$headers);
+
+foreach($csvs as $csv) {
+	
+	$ihandle	=	fopen('merge/'.$csv,'r');
+	
+	$count	=	0;
+	
+	while( !feof($ihandle) ) {
+	
+		$row	=	fgetcsv($ihandle);
+		
+		if($count != 0) {
+		
+			$zip3	=	substr($row[3],0,3);
+			
+			if( in_array($zip3, $include) ) {
+		
+				$out	=	array(
+					$row[1],
+					$row[2],
+					$row[3],
+					$zip3,
+					$row[4],
+					$row[5],
+					$row[6],
+					$row[9],
+					$row[10],
+					$row[17]
+				);
+			
+				fputcsv($ohandle,$out);
+			}
+		}
+		
+		
+		$count++;
+		$totalcount++;
+	}
+	
+}
+
+
+fclose($ohandle);
+
+?>
